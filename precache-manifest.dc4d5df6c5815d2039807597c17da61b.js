154cf47863166f5972f6a14027608a7d4f7951c0
Dominic Ngalo <otiendominic@gmail.com>
2020-02-28 13:29:09 +0300

    Updates

---
+++ precache-manifest.dc4d5df6c5815d2039807597c17da61b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1af651c386317730c0e1ce17347cf40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8f9aee5237f271bb5c2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "439389f21ec04054c2c4",
+    "url": "/static/js/2.a2320f11.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a2320f11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f9aee5237f271bb5c2",
+    "url": "/static/js/main.69f76b6d.chunk.js"
+  },
+  {
+    "revision": "52bc7a87efecc049dd74",
+    "url": "/static/js/runtime-main.3bc3b0b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
